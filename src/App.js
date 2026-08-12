f7931ee39d3f4bb12d60d72dba4018f694d1e685
JoseSalviati <Jose@Salviatis.com>
2026-08-11 17:52:51 -0700

    Moved comment

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          //Edit <code>src/App.js</code> and save to reload.
-          Hello from Jose!
+            Hello from Jose!
         </p>
         <a
           className="App-link"
@@ -24,3 +23,4 @@ function App() {
 }
 
 export default App;
+//Edit <code>src/App.js</code> and save to reload.
